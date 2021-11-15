ff70dad6c0748386f97bfd450dd589523d683883
tonybwatson <tonybwatson@gmail.com>
2021-11-14 20:46:32 -0500

    author, book, book-author added

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,7 @@
 <?php
 
+use App\Http\Controllers\AuthorsController;
+use App\Http\Controllers\BooksController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -13,4 +15,13 @@ use Illuminate\Support\Facades\Route;
 | is assigned the "api" middleware group. Enjoy building your API!
 |
 */
+
+Route::middleware('auth:api')->prefix('v1')->group(function() {
+    Route::get('/user', function (Request $request){
+        return $request->user();
+    });
+
+    Route::apiResource('/authors', AuthorsController::class);
+    Route::apiResource('/books', BooksController::class);
+});
 
